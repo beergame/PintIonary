6a09dfbafec6139151852a408d50556e9dd7029c
Victor Balssa <victor@adback.co>
2017-10-06 12:16:30 +0200

    add studio new app arch

---
+++ app/src/main/java/tk/victor_balssa/pintionary/app/MainActivity.java
@@ -0,0 +1,13 @@
+package tk.victor_balssa.pintionary.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
