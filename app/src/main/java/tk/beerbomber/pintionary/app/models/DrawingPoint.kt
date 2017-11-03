cac0486ba8d8c18ba53591279f260011db6061fd
Victor Balssa <victor@adback.co>
2017-11-03 15:32:49 +0100

    misc

--- app/src/main/java/tk/beerbomber/pintionary/app/models/DrawingPoint.kt
+++ app/src/main/java/tk/beerbomber/pintionary/app/models/DrawingPoint.kt
@@ -3,6 +3,6 @@ package tk.beerbomber.pintionary.app.models
 import io.realm.RealmObject
 
 open class DrawingPoint : RealmObject() {
-    var x: Double = 0.toDouble()
-    var y: Double = 0.toDouble()
+    var x: Double = 0.0
+    var y: Double = 0.0
 }
