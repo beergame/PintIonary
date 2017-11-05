b3c0f186e21e2b1e6b7b14cddbb27dcb0e95acb4
victorbalssa <vic.130@hotmail.fr>
2017-11-05 16:40:15 +0100

    update icon res

--- app/src/main/java/tk/beergame/pintionary/app/MainActivity.kt
+++ app/src/main/java/tk/beergame/pintionary/app/MainActivity.kt
@@ -10,7 +10,6 @@ import android.support.v7.app.AppCompatActivity
 import android.view.MotionEvent
 import android.view.SurfaceHolder
 import android.view.SurfaceView
-import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import java.util.HashMap
